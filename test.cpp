e5ef856fe15641f795963d533af062acbede1abc
Chris Nagy <chrisnagy10@yahoo.com>
2019-02-03 20:05:52 -0600

    init

---
+++ test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+using namespace std;
+
+void run_philosopher(int threadNum){
+  printf("I am thread %d", threadNum);
+}
+
+void run_waiter(){
+  printf("I am waiter");
+}
+
+int main(){
+  cout<<"Hello world";
+  vector <std::thread> threads;
+  int numPhilosophers = 5;
+
+// spawn philosophers
+  for (int i = 0; i < numPhilosophers; i++){
+    threads.push_back(thread(run_philosopher, i));
+  }
+
+
+//spawn waiter
+  std::thread waiter(run_waiter);
+
+// join threads
+  for (int i = 0; i < threads.size(); i++){
+    threads.at(i).join();
+  }
+
+  waiter.join();
+
+  printf("Finished joining. Main done.");
+  return 0;
+}
