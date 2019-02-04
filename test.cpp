d316dde290f8ec77cf4c6688dd696ba0c7c11225
Chris Nagy <chrisnagy10@yahoo.com>
2019-02-03 20:41:50 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,12 +5,45 @@
 
 using namespace std;
 
+mutex LOCK;
+boolean FORKS = [true, true, true, true, true];
+int NUM_PHILOSOPHERS = 5;
 void run_philosopher(int threadNum){
-  printf("I am thread %d", threadNum);
+  int hunger = 100;
+  while (hunger > 0) {
+    LOCK.lock();
+    printf("Philosopher %d picking up forks", threadNum);
+    if (FORKS[threadNum - 1]){
+      printf("Philosopher %d picked up fork %d", threadNum, threadNum - 1);
+      FORKS[threadNum - 1] = false;
+    } else {
+      printf("Philosopher %d fork not available", threadNum);
+      return;
+    }
+    if (FORKS[threadNum % NUM_PHILOSOPHERS]){
+      printf("Philosopher %d picked up fork %d", threadNum, threadNum % NUM_PHILOSOPHERS);
+      FORKS[threadNum % NUM_PHILOSOPHERS] = false;
+    } else {
+      printf("Philosopher %d fork not available", threadNum);
+      printf("Philosopher %d putting back fork %d", threadNum, threadNum - 1);
+      FORKS[threadNum - 1] = true;
+    }
+
+    printf("Philosopher %d eating", threadNum);
+    hunger -= 10;
+    printf("Philosopher %d putting down forks", threadNum);
+
+    FORKS[threadNum - 1] = true;
+    FORKS[threadNum % NUM_PHILOSOPHERS] = true;
+
+    LOCK.unlock();
+  }
+  printf("Philosopher %d done eating", threadNum);
+  return;
 }
 
 void run_waiter(){
-  printf("I am waiter");
+
 }
 
 int main(){
